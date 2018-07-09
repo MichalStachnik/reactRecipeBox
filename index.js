01c8ed96c36ffdd8cb467f6b3f196a9d6391f873
Michal <Michal.stachnik@my.liu.edu>
2018-07-09 08:07:03 -0400

    fixed default route

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ mongoose.connect(db)
   .then(() => console.log('mongo database connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-  res.send({hi: 'hi'});
-});
 
 app.use('/recipes', recipes);
 
 if(process.env.NODE_ENV === 'production') {
+
   app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
+  
 }
 
 const PORT = process.env.PORT || 3000;
